5b736c0ce6c67f227bb58576e6ba25674b23a9c3
hhung2710 <tranhuyhung2710@gmail.com>
2025-05-24 03:21:10 +0700

    Finish header doujinshi

---
+++ src/app/doujinshi/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import DoujinshiLayout from "@/components/doujinshi-layout/doujinshi-layout";
+
+
+const DoujinshiPage = () => {
+    return (
+        <DoujinshiLayout >
+            <>hehe</>
+        </DoujinshiLayout>
+    );
+};
+
+export default DoujinshiPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 'use client'
 
-import { Flex, Text, Button } from "@radix-ui/themes";
+import { Button, Flex, Text } from "@radix-ui/themes";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
+
   return (
-    <Flex direction="column" gap="2">
-			<Text>Hello from Radix Themes :)</Text>
-			<Button onClick={() => router.push('/sign-in')}>Let&apos;s go</Button>
-		</Flex>
+    <Flex direction="column" gap="5">
+      <Text>Hello from Radix Themes :)</Text>
+      <Button onClick={() => router.push('/sign-in')}>Let&apos;s go</Button>
+      <div className="flex justify-center">
+          <Button
+            className="max-w-3xs"
+            onClick={() => router.push('/doujinshi')}
+            color="crimson"
+          >
+            Doujinshi
+          </Button>
+        </div>
+    </Flex>
   );
 }
