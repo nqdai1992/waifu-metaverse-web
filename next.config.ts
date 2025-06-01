041dcc095732fb2f9640d2519437df11503782c5
nqdai1992 <nguyendai1992@gmail.com>
2025-06-02 02:58:32 +0700

    Update domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  domains: ["localhost", "waifu-metaverse-jhfvs9ui5-nguyenquocdais-projects.vercel.app"],
 };
 
 export default nextConfig;
