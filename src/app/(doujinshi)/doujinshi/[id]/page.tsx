5ba512a7d5768727c348ece3655f93481a7b7985
nqdai1992 <nguyendai1992@gmail.com>
2025-06-02 15:03:44 +0700

    Fixing file name casing

--- src/app/(doujinshi)/doujinshi/[id]/page.tsx
+++ src/app/(doujinshi)/doujinshi/[id]/page.tsx
@@ -4,7 +4,7 @@ import { IDoujinshiDetail } from "@/components/doujinshi/doujinshi-card/doujinsh
 import DoujinshiDetail from "@/components/doujinshi/doujinshi-detail/doujinshi-detail";
 import DoujinshiLayout from "@/components/doujinshi/doujinshi-layout/doujinshi-layout";
 import HorizontalScrollSection from "@/components/doujinshi/horizontal-scroll-section/horizontal-scroll-section";
-import { useLikedItems } from "@/hooks/useLikedItems";
+import { useLikedItems } from "@/hooks/use-liked-items";
 import { newUploads, popularItems } from "@/mock-data/doujinshi-data";
 import Image from "next/image";
 import { useParams, useRouter } from "next/navigation";
